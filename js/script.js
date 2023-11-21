9970791e06212558316d7acbe47f7e8d497fcaf3
Andrea9808 <andrea.manigrasso98@gmail.com>
2023-11-21 14:43:08 +0100

    rimossa variabile in console.log

--- js/script.js
+++ js/script.js
@@ -10,17 +10,17 @@ for (let i = 1; i <= 100; i++){
     // calcoliamo i numeri divisibili *3 e *5 
     if((i % 3 === 0) && (i % 5 === 0)){
 
-        console.log("FizzBuzz" + i);
+        console.log("FizzBuzz");
     
     // calcoliamo i numeri divisibili *3
     }else if(i % 3 === 0){
 
-        console.log("Fizz" + i);
+        console.log("Fizz");
     
     // calcoliamo i numeri divisibili *5
     }else if(i % 5 === 0){
 
-        console.log("Buzz" + i);
+        console.log("Buzz");
 
     //inseriamo i numeri non divisibili *3 *5 ed entrambi
     }else{
